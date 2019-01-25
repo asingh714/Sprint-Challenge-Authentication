4db389b8707ef6073ae9eb22dce34f001a65db04
Aman <amsingh714@gmail.com>
2019-01-25 11:49:54 -0500

    completed login function

--- config/routes.js
+++ config/routes.js
@@ -1,61 +1,72 @@
-const axios = require('axios');
+const axios = require("axios");
 const bcrypt = require("bcryptjs");
 
-const { authenticate, generateToken } = require('../auth/authenticate');
+const { authenticate, generateToken } = require("../auth/authenticate");
 
-const db = require("../database/dbConfig")
-
+const db = require("../database/dbConfig");
 
 module.exports = server => {
-  server.post('/api/register', register);
-  server.post('/api/login', login);
-  server.get('/api/jokes', authenticate, getJokes);
+  server.post("/api/register", register);
+  server.post("/api/login", login);
+  server.get("/api/jokes", authenticate, getJokes);
 };
 
-
-
-
-
 function register(req, res) {
-  // implement user registration
-  const userInfo = req.body; 
+  const userInfo = req.body;
 
   const hash = bcrypt.hashSync(userInfo.password, 14);
 
-  userInfo.password = hash
+  userInfo.password = hash;
 
   db("users")
-  .insert(userInfo)
-  .then(id => {
-    db("users")
-    .where({ username: userInfo.username})
-    .select("id", "username")
-    .first()
-    .then(user => {
-      let token = generateToken(user);
-      res.status(201).json({ user, token })
+    .insert(userInfo)
+    .then(id => {
+      db("users")
+        .where({ username: userInfo.username })
+        .select("id", "username")
+        .first()
+        .then(user => {
+          let token = generateToken(user);
+          res.status(201).json({ user, token });
+        });
     })
-  })
-  .catch(err => {
-    res.status(500).json({ error: "There was an error while creating the user. "})
-  })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "There was an error while creating the user. " });
+    });
 }
 
 function login(req, res) {
-  // implement user login
+  const credentials = req.body;
+
+  db("users")
+    .where({ username: credentials.username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(credentials.password, user.password)) {
+        const token = generateToken(user);
+        res
+          .status(200)
+          .json({ message: `${user.username} is logged in`, token });
+      } else {
+        res.status(401).json({ message: "Sorry you are not authorized." });
+      }
+    })
+    .catch(err => res.status(500).json(err));
 }
 
 function getJokes(req, res) {
   const requestOptions = {
-    headers: { accept: 'application/json' },
+    headers: { accept: "application/json" }
   };
 
   axios
-    .get('https://icanhazdadjoke.com/search', requestOptions)
+    .get("https://icanhazdadjoke.com/search", requestOptions)
     .then(response => {
       res.status(200).json(response.data.results);
     })
     .catch(err => {
-      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
+      res.status(500).json({ message: "Error Fetching Jokes", error: err });
     });
 }
