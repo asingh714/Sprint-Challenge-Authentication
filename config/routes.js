77871ed7be1fc0a2a49f2a3c70f47477d3ef8871
Aman <amsingh714@gmail.com>
2019-04-05 11:00:42 -0400

    intial commit

--- config/routes.js
+++ config/routes.js
@@ -1,72 +1,32 @@
-const axios = require("axios");
-const bcrypt = require("bcryptjs");
+const axios = require('axios');
 
-const { authenticate, generateToken } = require("../auth/authenticate");
-
-const db = require("../database/dbConfig");
+const { authenticate } = require('../auth/authenticate');
 
 module.exports = server => {
-  server.post("/api/register", register);
-  server.post("/api/login", login);
-  server.get("/api/jokes", authenticate, getJokes);
+  server.post('/api/register', register);
+  server.post('/api/login', login);
+  server.get('/api/jokes', authenticate, getJokes);
 };
 
 function register(req, res) {
-  const userInfo = req.body;
-
-  const hash = bcrypt.hashSync(userInfo.password, 14);
-
-  userInfo.password = hash;
-
-  db("users")
-    .insert(userInfo)
-    .then(id => {
-      db("users")
-        .where({ username: userInfo.username })
-        .select("id", "username")
-        .first()
-        .then(user => {
-          let token = generateToken(user);
-          res.status(201).json({ user, token });
-        });
-    })
-    .catch(err => {
-      res
-        .status(500)
-        .json({ error: "There was an error while creating the user. " });
-    });
+  // implement user registration
 }
 
 function login(req, res) {
-  const credentials = req.body;
-
-  db("users")
-    .where({ username: credentials.username })
-    .first()
-    .then(user => {
-      if (user && bcrypt.compareSync(credentials.password, user.password)) {
-        const token = generateToken(user);
-        res
-          .status(200)
-          .json({ message: `${user.username} is logged in`, token });
-      } else {
-        res.status(401).json({ message: "Sorry you are not authorized." });
-      }
-    })
-    .catch(err => res.status(500).json(err));
+  // implement user login
 }
 
 function getJokes(req, res) {
   const requestOptions = {
-    headers: { accept: "application/json" }
+    headers: { accept: 'application/json' },
   };
 
   axios
-    .get("https://icanhazdadjoke.com/search", requestOptions)
+    .get('https://icanhazdadjoke.com/search', requestOptions)
     .then(response => {
       res.status(200).json(response.data.results);
     })
     .catch(err => {
-      res.status(500).json({ message: "Error Fetching Jokes", error: err });
+      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
     });
 }
