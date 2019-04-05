1aea7af9054f3d13e21ffd849171ec3eb89c96f5
Aman <amsingh714@gmail.com>
2019-04-05 11:19:27 -0400

    able to generate token and register new user

--- config/routes.js
+++ config/routes.js
@@ -1,15 +1,44 @@
-const axios = require('axios');
+const axios = require("axios");
+const bcrypt = require("bcryptjs");
 
-const { authenticate } = require('../auth/authenticate');
+const { authenticate } = require("../auth/authenticate");
+const UsersDB = require("../database/dbConfig");
+const tokenService = require("../auth/token-service");
 
 module.exports = server => {
-  server.post('/api/register', register);
-  server.post('/api/login', login);
-  server.get('/api/jokes', authenticate, getJokes);
+  server.post("/api/register", register);
+  server.post("/api/login", login);
+  server.get("/api/jokes", authenticate, getJokes);
 };
 
 function register(req, res) {
-  // implement user registration
+  const user = req.body;
+
+  if (!user.username || !user.password) {
+    res.status(400).json({
+      error: "Please provide a username and password."
+    });
+  } else {
+    const hash = bcrypt.hashSync(user.password, 10);
+    user.password = hash;
+    UsersDB("users")
+      .insert(user)
+      .then(ids => {
+        const id = ids[0];
+        UsersDB("users")
+          .where({ id })
+          .first()
+          .then(user => {
+            const token = tokenService.generateToken(user);
+            res.status(201).json({ user, token });
+          });
+      })
+      .catch(error => {
+        res.status(500).json({
+          error: "There was an error while saving the user to the database."
+        });
+      });
+  }
 }
 
 function login(req, res) {
@@ -18,15 +47,15 @@ function login(req, res) {
 
 function getJokes(req, res) {
   const requestOptions = {
-    headers: { accept: 'application/json' },
+    headers: { accept: "application/json" }
   };
 
   axios
-    .get('https://icanhazdadjoke.com/search', requestOptions)
+    .get("https://icanhazdadjoke.com/search", requestOptions)
     .then(response => {
       res.status(200).json(response.data.results);
     })
     .catch(err => {
-      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
+      res.status(500).json({ message: "Error Fetching Jokes", error: err });
     });
 }
